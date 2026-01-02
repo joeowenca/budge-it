d367fe9ae9823ed8375990fbc3c888568217401b
Joe Owen <joe.a.owen@gmail.com>
2026-01-02 08:49:03 -0800

    Make combobox popover align center

--- src/components/ui/combobox.tsx
+++ src/components/ui/combobox.tsx
@@ -117,7 +117,7 @@ export function Combobox({
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent align="start" style={{ width: "var(--radix-popover-trigger-width)" }} className="p-0">
+      <PopoverContent style={{ width: "var(--radix-popover-trigger-width)" }} className="p-0">
         <Command>
           <CommandInput
             placeholder={searchPlaceholder}
