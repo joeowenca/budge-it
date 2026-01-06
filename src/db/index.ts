57945f94c8bf91a1b72fcccb7b507dd5e533561f
Joe Owen <joe.a.owen@gmail.com>
2026-01-05 17:57:56 -0800

    Change database connection from Neon to Supabase

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,8 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+import postgres from "postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL!);
+const connectionString = process.env.DATABASE_URL!;
+const client = postgres(connectionString);
 
-export const db = drizzle(sql, { schema });
+export const db = drizzle(client, { schema });
