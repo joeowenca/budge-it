3e72b3fd2b1c84579e7aead16a931f55f7fe0e6f
Joe Owen <joe.a.owen@gmail.com>
2026-01-05 22:00:39 -0800

    Add new budget schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,10 @@
-import { integer, pgTable, pgEnum, serial, text, timestamp, boolean, uuid } from "drizzle-orm/pg-core";
+import { integer, pgTable, pgEnum, serial, text, timestamp, boolean, uuid, doublePrecision } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 
+export const budgetTypeEnum = pgEnum("budget_type", ["income", "expense", "savings"]);
+export const frequencyTypeEnum = pgEnum("frequency_type", ["weekly", "bi-weekly", "semi-monthly", "monthly"]);
+
+// DELET THIS
 export const transactionTypeEnum = pgEnum("transaction_type", ["income", "expense", "purchase"]);
 
 export const users = pgTable("users", {
@@ -13,6 +17,66 @@ export const users = pgTable("users", {
   theme: text("theme").default("system"),
   createdAt: timestamp("created_at").defaultNow(),
 });
+
+export const budgetCategories = pgTable("budget_categories", {
+  id: serial("id").primaryKey(),
+  userId: integer("user_id").references(() => users.id, { onDelete: "cascade" }).notNull(),
+  type: budgetTypeEnum("type").notNull(),
+  emoji: text("emoji"),
+  name: text("name").notNull(),
+  color: text("color"),
+  totalAmount: integer("total_amount").notNull().default(0),
+  sortOrder: doublePrecision("sort_order").notNull(),
+  isArchived: boolean("is_archived").default(false),
+  updatedAt: timestamp("updated_at").defaultNow(),
+  archivedAt: timestamp("archived_at"),
+  createdAt: timestamp("created_at").defaultNow()
+});
+
+export const budgetItems = pgTable("budget_items", {
+  id: serial("id").primaryKey(),
+  userId: integer("user_id").references(() => users.id, { onDelete: "cascade" }).notNull(),
+  budgetCategoryId: integer("budget_caftegory_id").references(() => budgetCategories.id, { onDelete: "cascade" }).notNull(),
+  type: budgetTypeEnum("type").notNull(),
+  name: text("name").notNull(),
+  amount: integer("amount").notNull().default(0),
+  frequency: frequencyTypeEnum("frequency").notNull(),
+  startDate: timestamp("start_date").notNull(),
+  dayOfWeek: text("day_of_week"),
+  dayOfMonth: text("day_of_month"),
+  secondDayOfMonth: text("second_day_of_month"),
+  sortOrder: doublePrecision("sort_order").notNull(),
+  isArchived: boolean("is_archived").default(false),
+  updatedAt: timestamp("updated_at").defaultNow(),
+  archivedAt: timestamp("archived_at"),
+  createdAt: timestamp("created_at").defaultNow()
+});
+
+export const usersRelations = relations(users, ({ many }) => ({
+  categories: many(categories),
+  transactions: many(transactions),
+  budgetCategories: many(budgetCategories),
+  budgetItems: many(budgetItems)
+}));
+
+export const budgetCategoryRelations = relations(budgetCategories, ({ one, many }) => ({
+  user: one(users, {
+    fields: [budgetCategories.userId],
+    references: [users.id],
+  }),
+  budgetItems: many(budgetItems)
+}));
+
+export const budgetItemRelations = relations(budgetItems, ({ one }) => ({
+  user: one(users, {
+    fields: [budgetItems.userId],
+    references: [users.id],
+  }),
+  budgetCategory: one(budgetCategories, {
+    fields: [budgetItems.budgetCategoryId],
+    references: [budgetCategories.id]
+  })
+}));
 
 export const categories = pgTable("categories", {
   id: serial("id").primaryKey(),
@@ -39,11 +103,6 @@ export const transactions = pgTable("transactions", {
   isArchived: boolean("is_archived").default(false),
   createdAt: timestamp("created_at").defaultNow(),
 });
-
-export const usersRelations = relations(users, ({ many }) => ({
-  categories: many(categories),
-  transactions: many(transactions),
-}));
 
 export const categoriesRelations = relations(categories, ({ one, many }) => ({
   user: one(users, {
