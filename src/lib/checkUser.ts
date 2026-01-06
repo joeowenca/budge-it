b43529a3208af4cdde7ebdb6c20def4e5a956104
Joe Owen <joe.a.owen@gmail.com>
2026-01-05 20:30:03 -0800

    Refactor checkUser.ts to switch from Clerk to Supabase Auth

--- src/lib/checkUser.ts
+++ src/lib/checkUser.ts
@@ -1,32 +1,20 @@
-import { currentUser } from "@clerk/nextjs/server";
+import { createClient } from "@/utils/supabase/server";
 import { db } from "@/db";
 import { users } from "@/db/schema";
 import { eq } from "drizzle-orm";
 
 export const checkUser = async () => {
-  const user = await currentUser();
+  const supabase = await createClient();
+  
+  const { data: { user } } = await supabase.auth.getUser();
 
   if (!user) {
     return null;
   }
 
   const loggedInUser = await db.query.users.findFirst({
-    where: eq(users.clerkId, user.id),
+    where: eq(users.authId, user.id),
   });
 
-  if (loggedInUser) {
-    return loggedInUser;
-  }
-
-  const newUser = await db
-    .insert(users)
-    .values({
-      clerkId: user.id,
-      name: `${user.firstName} ${user.lastName}`,
-      imageUrl: user.imageUrl,
-      email: user.emailAddresses[0].emailAddress,
-    })
-    .returning();
-
-  return newUser[0];
+  return loggedInUser || null;
 };
